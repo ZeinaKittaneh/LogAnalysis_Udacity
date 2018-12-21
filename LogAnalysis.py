dcb5f9dda6e9d64d010a819eaa12fe83fc0a5c85
ZeinaKittaneh <36554989+ZeinaKittaneh@users.noreply.github.com>
2018-12-21 14:31:37 -0500

    Update LogAnalysis.py

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -1,65 +1,118 @@
-#!/usr/bin/env python3
+#! /usr/bin/python2
 
 import psycopg2
 
 DBNAME = "news"
-q1 = "\nWhat are the most popular three articles of all time?\n"
-ans1 = '''select title, count(substr(path, 10)) as views from articles join log
-on slug = substr(path, 10) group by title order by views desc limit 3;'''
+question1 = "\nWhat are the most popular three articles of all time?\n"
+query1 = '''SELECT
+    title,
+    COUNT(substr(path, 10)) AS views
+FROM
+    articles
+    JOIN
+        log
+        ON slug = substr(path, 10)
+GROUP BY
+    title
+ORDER BY
+    views DESC LIMIT 3;'''
 
-q2 = "\nWho are the most popular article authors of all time?\n"
-ans2 = '''select auth.name, sum(views_qry.views) from authors auth,
-articles artic, ( select title, count(substr(path, 10)) as views from articles
-join log on slug = substr(path, 10) group by title) views_qry where
-auth.id = artic.author and views_qry.title = artic.title group by auth.name
-order by sum(views_qry.views) desc;'''
+question2 = "\nWho are the most popular article authors of all time?\n"
+query2 = '''SELECT
+    auth.name,
+    SUM(views_qry.views)
+FROM
+    authors auth,
+    articles artic,
+    (
+        SELECT
+            title,
+            COUNT(substr(path, 10)) AS views
+        FROM
+            articles
+            JOIN
+                log
+                ON slug = substr(path, 10)
+        GROUP BY
+            title
+    )
+    views_qry
+WHERE
+    auth.id = artic.author
+    AND views_qry.title = artic.title
+GROUP BY
+    auth.name
+ORDER BY
+    SUM(views_qry.views) DESC;'''
 
-q3 = "\nOn which days did more than 1% of requests lead to errors?\n"
-ans3 = '''select * from (
-select total.day,
-round(cast((error.errorCount*100) as numeric) / cast(total.totalCount
-as numeric), 2) as percentage from
-(select date(time) as day, count(*) as errorCount from log where status
-not like '%200 OK%' group by day) as error
-inner join
-(select  date(time) as day, count(*) as totalCount from log group by day)
-as total on total.day = error.day) as subqry
- where percentage > 1.0;'''
+question3 = "\nOn which days did more than 1% of requests lead to errors?\n"
+query3 = '''SELECT
+    *
+FROM
+    (
+        SELECT
+            total.DAY,
+            round(CAST((error.errorCount*100) AS NUMERIC) /
+            CAST(total.totalCount AS NUMERIC), 2) AS percentage
+        FROM
+            (
+                SELECT
+                    DATE(TIME) AS DAY,
+                    COUNT(*) AS errorCount
+                FROM
+                    log
+                WHERE
+                    status NOT LIKE '%200 OK%'
+                GROUP BY
+                    DAY
+            )
+            AS error
+            INNER JOIN
+                (
+                    SELECT
+                        DATE(TIME) AS DAY,
+                        COUNT(*) AS totalCount
+                    FROM
+                        log
+                    GROUP BY
+                        DAY
+                )
+                AS total
+                ON total.DAY = error.DAY
+    )
+    AS subqry
+WHERE
+    percentage > 1.0;'''
 
 
-def solve_q1():
-    conn = psycopg2.connect(dbname=DBNAME)
-    cursor = conn.cursor()
-    print q1
-    cursor.execute(ans1)
+def get_top_articles(cursor):
+    print (question1)
+    cursor.execute(query1)
     results = cursor.fetchall()
     for result in results:
         print ('\t' + str(result[0]) + ' - ' + str(result[1]) + ' views')
-    conn.close()
 
 
-def solve_q2():
-    conn = psycopg2.connect(dbname=DBNAME)
-    cursor = conn.cursor()
-    print(q2)
-    cursor.execute(ans2)
+def get_popular_author(cursor):
+    print(question2)
+    cursor.execute(query2)
     results = cursor.fetchall()
     for result in results:
         print ('\t' + str(result[0]) + ' - ' + str(result[1]) + ' views')
-    conn.close()
 
 
-def solve_q3():
-    conn = psycopg2.connect(dbname=DBNAME)
-    cursor = conn.cursor()
-    print(q3)
-    cursor.execute(ans3)
+def get_day_max_error(cursor):
+    print(question3)
+    cursor.execute(query3)
     results = cursor.fetchall()
     for result in results:
         print ('\t' + str(result[0]) + ' - ' + str(result[1]) + '%')
-    conn.close()
+
 
 if __name__ == '__main__':
-    solve_q1()
-    solve_q2()
-    solve_q3()
+    conn = psycopg2.connect(dbname=DBNAME)
+    cursor = conn.cursor()
+    get_top_articles(cursor)
+    get_popular_author(cursor)
+    get_day_max_error(cursor)
+    conn.close()
